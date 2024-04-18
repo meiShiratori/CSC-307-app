ddbdc879824fdd222db5925687ccb35037e7a6ac
meiShiratori <tshirato@calpoly.edu>
2024-04-18 16:02:34 -0700

    added ability to add users to backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,6 +41,11 @@ const findUserByName = (name) => {
 
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
+  
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
 
 app.use(express.json());
 
@@ -77,4 +82,10 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.send(result);
   }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
